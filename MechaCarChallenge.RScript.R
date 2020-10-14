46f6fc4d41bc93755b8593d9046df66c1851d994
Asad Khan <asad_ca4u@hotmail.com>
2020-10-14 15:42:52 -0400

    Completed Deliverable 3

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -10,10 +10,12 @@ total_summary <- SuspensionDF %>% summarize(Mean=mean(PSI), Median=median(PSI), 
 lot_summary <- SuspensionDF %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 
 #Deliverable 3
+#Across all three lots
+t.test(SuspensionDF$PSI, mu=1500) 
 #Lot 1
-t.test(SuspensionDF$PSI, mu=1500, subset(SuspensionDF, Manufacturing_Lot == "Lot1", select=PSI))
+t.test(subset(SuspensionDF, Manufacturing_Lot == "Lot1", select=PSI), mu=1500)
 #Lot 2
-t.test(SuspensionDF$PSI, mu=1500, subset(SuspensionDF, Manufacturing_Lot == "Lot2", select=PSI))
+t.test(subset(SuspensionDF, Manufacturing_Lot == "Lot2", select=PSI), mu=1500)
 #Lot 3
-t.test(SuspensionDF$PSI, mu=1500, subset(SuspensionDF, Manufacturing_Lot == "Lot3", select=PSI))
+t.test(subset(SuspensionDF, Manufacturing_Lot == "Lot3", select=PSI), mu=1500)
 

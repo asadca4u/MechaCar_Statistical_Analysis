3452962d395bb79dbbe0a0d729f868c36ba0e2ce
Asad Khan <asad_ca4u@hotmail.com>
2020-10-14 13:54:00 -0400

    completed Deliverable 2

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,4 +1,10 @@
+#Deliverable 1
 library(dplyr)
 MechaCarDF <- read.csv(file='data/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 LinearRegression1 <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCarDF)
 summary(LinearRegression1)
+
+#Deliverable 2
+SuspensionDF <- read.csv(file='data/Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+total_summary <- SuspensionDF %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+lot_summary <- SuspensionDF %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))

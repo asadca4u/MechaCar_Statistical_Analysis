ba95bb2c6f552c7da54371b0fe2307aa2b23843a
Asad Khan <asad_ca4u@hotmail.com>
2020-10-14 13:58:16 -0400

    Completed Deliverable 2

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,5 +1,6 @@
-#Deliverable 1
 library(dplyr)
+
+#Deliverable 1
 MechaCarDF <- read.csv(file='data/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 LinearRegression1 <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCarDF)
 summary(LinearRegression1)
